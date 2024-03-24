53b110e2acb0ce604e2294eaef8ccb125bb51823
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2024-03-24 03:23:04 +0300

    feat: implement getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -76,8 +76,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  let days = 5 - date.getDay();
+  days = days <= 0 ? days + 7 : days;
+  date.setDate(date.getDate() + days);
+  return date;
 }
 
 /**
